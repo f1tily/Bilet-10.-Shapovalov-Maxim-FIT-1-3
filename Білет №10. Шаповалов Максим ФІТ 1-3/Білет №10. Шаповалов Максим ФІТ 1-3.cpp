cc89f2bf0c2856eb165499a9293ffef97cb5b132
f1tily <maximshapovalov142@gmail.com>
2023-12-03 14:09:57 +0200

    Finish work

---
+++ Білет №10. Шаповалов Максим ФІТ 1-3/Білет №10. Шаповалов Максим ФІТ 1-3.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+    void sort_row(int row[], int size) {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (row[j] > row[j + 1]) {
+                    int temp = row[j];
+                    row[j] = row[j + 1];
+                    row[j + 1] = temp;
+                }
+            }
+        }
+    }
+    int main() {
+        setlocale(LC_ALL, "Ukrainian");
+        int array[15][2];
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 2; j++) {
+                cout << "Введіть елемент [" << i << "][" << j << "]: ";
+                cin >> array[i][j];
+            }
+        }
+
+        for (int i = 0; i < 15; i++) {
+            sort_row(array[i], 2);
+        }
+
+        int min_sum = array[0][0] + array[1][0];
+        int min_row1 = 0;
+        int min_row2 = 1;
+        for (int i = 1; i < 14; i++) {
+            int sum = array[i][0] + array[i + 1][0];
+            if (sum < min_sum) {
+                min_sum = sum;
+                min_row1 = i;
+                min_row2 = i + 1;
+            }
+        }
+
+        cout << "Номер першого рядка: " << min_row1 << endl;
+        cout << "Номер другого рядка: " << min_row2 << endl;
+        return 0;
+    }
